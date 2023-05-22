2dd97ea94b88964cf7960694d851851b8f124769
Huang Weijie <h_weijie@yahoo.com>
2023-05-21 21:50:30 -0700

    feat: fiddling with zsh

--- hosts/aldehyde/default.nix
+++ hosts/aldehyde/default.nix
@@ -74,22 +74,26 @@
     docker-desktop.enable = true;
   };
 
-  # # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
-  # users.users = {
-  #   # FIXME: Replace with your username
-  #   your-username = {
-  #     # TODO: You can set an initial password for your user.
-  #     # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-  #     # Be sure to change it (using passwd) after rebooting!
-  #     initialPassword = "correcthorsebatterystaple";
-  #     isNormalUser = true;
-  #     openssh.authorizedKeys.keys = [
-  #       # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-  #     ];
-  #     # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-  #     extraGroups = [ "wheel" ];
-  #   };
-  # };
+  environment = {
+    systemPackages = [
+      pkgs.python3
+    ];
+
+    shells = [ pkgs.zsh ];
+  };
+
+  programs.zsh.enable = true;
+
+  users.defaultUserShell = pkgs.zsh;
+
+  users.users = {
+    weijie = {
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      ];
+    };
+  };
 
   services.openssh = {
     enable = true;
