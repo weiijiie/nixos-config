857f5f8f5bd6f02ffd913ef88d580e9e576e93ca
Huang Weijie <h_weijie@yahoo.com>
2023-05-21 22:09:40 -0700

    feat: add public ssh key

--- hosts/aldehyde/default.nix
+++ hosts/aldehyde/default.nix
@@ -90,7 +90,7 @@
     weijie = {
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+        (builtins.readFile ../../home/ssh.pub)
       ];
     };
   };
