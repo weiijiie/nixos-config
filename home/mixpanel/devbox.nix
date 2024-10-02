a50e4db4de27afdef1f69bdff8f8a3a54d5de4a2
weijie-mxpl <weijie.huang@mixpanel.com>
2024-10-01 22:30:13 -0700

    fix: stupid mistakes

--- home/mixpanel/devbox.nix
+++ home/mixpanel/devbox.nix
@@ -1,21 +1,35 @@
-{ ... }@args:
-(common: common // { 
-  home.packges = [
-    manix
-    tree
-    yq-go
-    tokei
-    ranger
-    tldr
-    bottom
-    nil
-    nixfmt-classic
-  ];
+# disable some of the config because it has already been
+# configured in the devbox during provisioning
+{ pkgs, ... }@args:
+((common: common // {
+  home = common.home // {
+    packages = with pkgs; [
+      manix
+      tree
+      yq-go
+      tokei
+      ranger
+      tldr
+      bottom
+      nil
+      nixfmt-classic
+    ];
+  };
 
-  go.enable = false;
+  programs = common.programs // {
+    ssh.enable = false;
+    go.enable = false;
 
-  programs.zsh.initExtra = common.programs.zsh.initExtra + "\n" + ''
-  # devbox setup
-  source ~/.gcpdevbox
-  '';
-} (import ../common.nix args))
+    zsh = common.programs.zsh // {
+      initExtra = common.programs.zsh.initExtra + "\n" + ''
+        # devbox setup
+        source ~/.gcpdevbox
+        source ~/analytics/google-cloud/scripts/kube.sh
+      '';
+
+      envExtra = ''
+        source $HOME/analytics/.shellenv
+      '';
+    };
+  };
+}) (import ../common.nix args))
