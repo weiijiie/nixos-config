ae539f420c03f1fb4eb1c22229a38825b38249c4
Huang Weijie <43085321+weiijiie@users.noreply.github.com>
2024-10-09 07:43:58 +0000

    feat: globally gitignore devbox custom editorconfig

--- home/mixpanel/devbox.nix
+++ home/mixpanel/devbox.nix
@@ -1,36 +1,39 @@
 # disable some of the config because it has already been
 # configured in the devbox during provisioning
 { pkgs, ... }@args:
-((common: common // {
-  home = common.home // {
-    packages = with pkgs; [
-      manix
-      tree
-      yq-go
-      tokei
-      ranger
-      tldr
-      bottom
-      nil
-      nixfmt-classic
-      bazel-buildtools
-    ];
-  };
-
-  programs = common.programs // {
-    ssh.enable = false;
-    go.enable = false;
-
-    zsh = common.programs.zsh // {
-      initExtra = common.programs.zsh.initExtra + "\n" + ''
-        # devbox setup
-        source ~/.gcpdevbox
-        source ~/analytics/google-cloud/scripts/kube.sh
-      '';
-
-      envExtra = ''
-        source $HOME/analytics/.shellenv
-      '';
+((common:
+  common // {
+    home = common.home // {
+      packages = with pkgs; [
+        manix
+        tree
+        yq-go
+        tokei
+        ranger
+        tldr
+        bottom
+        nil
+        nixfmt-classic
+        bazel-buildtools
+      ];
     };
-  };
-}) (import ../common.nix args))
+
+    programs = common.programs // {
+      ssh.enable = false;
+      go.enable = false;
+
+      git = common.programs.git // { ignores = [ "/go/.editorconfig" ]; };
+
+      zsh = common.programs.zsh // {
+        initExtra = common.programs.zsh.initExtra + "\n" + ''
+          # devbox setup
+          source ~/.gcpdevbox
+          source ~/analytics/google-cloud/scripts/kube.sh
+        '';
+
+        envExtra = ''
+          source $HOME/analytics/.shellenv
+        '';
+      };
+    };
+  }) (import ../common.nix args))
