c76728f5c608f164dc1c20be27f597045b9d03b9
Huang Weijie <43085321+weiijiie@users.noreply.github.com>
2026-04-08 08:50:51 +0000

    fix: git user warnings

--- home/personal.nix
+++ home/personal.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
-  programs.git = {
-    userEmail = "43085321+weiijiie@users.noreply.github.com";
-    userName = "Huang Weijie";
+  programs.git.settings.user = {
+    email = "43085321+weiijiie@users.noreply.github.com";
+    name = "Huang Weijie";
   };
 
   home.packages = with pkgs; [ imagemagickBig ];
